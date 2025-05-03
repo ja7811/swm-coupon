dc179c53e67e574e57e8f2a950996adeed81f1f3
JaehwanH <jec0101@naver.com>
2025-05-03 12:43:37 +0900

    Initial Commit

---
+++ src/main/java/com/swm/coupon/SwmCouponApplication.java
@@ -0,0 +1,13 @@
+package com.swm.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwmCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwmCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swm/coupon/SwmCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwmCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
